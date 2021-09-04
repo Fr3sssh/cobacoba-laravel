08ea36d94e7e0ee564eb444458466ea2c5e8ebe0
Fr3sssh <indraathalla@upi.edu>
2021-09-04 23:03:47 +0700

    initial feature to the backend, added login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+
+@section('title','loginpage')
+
+@section('content')
+    <div>
+        name: <input type="text" name="email"><br>
+        password: <input type="password" name="password"><br>
+    </div>
+@endsection
